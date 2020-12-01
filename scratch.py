a1ebaddf1d6217f1d8a5b3938cfcca990e2922b2
Patrick Gingras <775.pg.12@gmail.com>
2020-12-01 01:18:06 -0500

    added scratch file

---
+++ scratch.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import numpy as np
+import os
+import re
+import sys
+from collections import ChainMap, defaultdict, deque
+from dataclasses import dataclass
+from enum import Enum
+from functools import partial
+from itertools import chain, combinations, count, cycle, product, repeat
+from math import ceil, cos, cosh, dist, floor, gcd, hypot, sin, sinh, tan, tanh
+from operator import attrgetter, itemgetter, methodcaller
+
+
