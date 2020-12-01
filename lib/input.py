94338415fb1f4b567bfba7b37e29c5cc2497517d
Shen Li <dustet@gmail.com>
2020-11-30 22:34:49 -0500

    Make input importing  easier (#1)
    * make input importing easier
    
    * move utils to lib.input
    
    * refactor into different functions for lines
    
    Co-authored-by: Patrick Gingras <775.pg.12@gmail.com>

---
+++ lib/input.py
@@ -0,0 +1,20 @@
+import inspect
+from pathlib import Path
+
+
+def _get_input(nth_frame):
+    frame = inspect.stack()[nth_frame]
+    module = inspect.getmodule(frame[0])
+    input_path = Path(module.__file__).parent / "input.txt"
+    with open(input_path, "r") as f:
+        return f.read()
+
+
+def get_input(strip=True):
+    content = _get_input(1)
+    return content.strip() if strip else content
+
+
+def get_input_lines(strip=True):
+    lines = _get_input(1).splitlines()
+    return [l.strip() for l in lines] if strip else lines
