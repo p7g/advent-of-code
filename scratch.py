8a9c297d628455f990cc2998330cd42416827ae6
Patrick Gingras <775.pg.12@gmail.com>
2020-12-16 01:09:55 -0500

    more imports

--- scratch.py
+++ scratch.py
@@ -6,10 +6,10 @@ import sys
 from collections import ChainMap, defaultdict, deque, Counter
 from dataclasses import dataclass
 from enum import Enum
-from functools import partial
-from itertools import chain, combinations, count, cycle, product, repeat
+from functools import partial, reduce
+from itertools import chain, combinations, count, cycle, permutations, product, repeat
 from math import ceil, cos, cosh, dist, floor, gcd, hypot, sin, sinh, tan, tanh
-from operator import attrgetter, itemgetter, methodcaller
+from operator import and_, mul, attrgetter, itemgetter, methodcaller
 
 from lib.input import (
     fetch,
