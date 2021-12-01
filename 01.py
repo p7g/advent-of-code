5da6e2d94400592d9f4754f0271249cf5832dea1
Patrick Gingras <775.pg.12@gmail.com>
2021-12-01 00:23:40 -0500

    make simple

--- 01.py
+++ 01.py
@@ -1,8 +1,18 @@
-from collections import deque
-from patina import None_, Some
-from lib.input import fetch_int_lines
+from aoc import *
 
-data = fetch_int_lines()
+data = list(map(int, data.splitlines()))
+
+prev = None_()
+inc = 0
+
+for n in data:
+    match prev:
+        case Some(p) if n > p:
+            inc += 1
+    prev.replace(n)
+
+print(inc)
+
 
 prevsum = None_()
 prevs = deque(data[:3], maxlen=3)
---
+++ aoc.py
@@ -0,0 +1,161 @@
+import datetime as dt
+import os
+import networkx as nx
+import numpy as np
+import re
+import sys
+import typing as t
+from collections import ChainMap, Counter, defaultdict, deque
+from dataclasses import dataclass
+from enum import Enum
+from functools import partial, reduce
+from itertools import chain, combinations, count, cycle, permutations, product, repeat
+from math import ceil, cos, cosh, dist, floor, gcd, hypot, sin, sinh, tan, tanh
+from operator import (
+    add,
+    and_,
+    attrgetter,
+    floordiv,
+    itemgetter,
+    methodcaller,
+    mod,
+    mul,
+    or_,
+    sub,
+    truediv,
+    xor,
+)
+from patina import Err, None_, Ok, Option, Result, Some
+
+
+data: str
+
+__all__ = [  # noqa
+    "ChainMap",
+    "Counter",
+    "Enum",
+    "Err",
+    "None_",
+    "Ok",
+    "Option",
+    "Result",
+    "Some",
+    "add",
+    "and_",
+    "attrgetter",
+    "ceil",
+    "chain",
+    "combinations",
+    "cos",
+    "cosh",
+    "count",
+    "cycle",
+    "data",
+    "dataclass",
+    "defaultdict",
+    "deque",
+    "dist",
+    "floor",
+    "floordiv",
+    "gcd",
+    "hypot",
+    "itemgetter",
+    "methodcaller",
+    "mod",
+    "mul",
+    "np",
+    "nx",
+    "or_",
+    "os",
+    "partial",
+    "permutations",
+    "product",
+    "re",
+    "reduce",
+    "repeat",
+    "sin",
+    "sinh",
+    "sub",
+    "sys",
+    "tan",
+    "tanh",
+    "truediv",
+    "xor",
+]
+
+
+def __getattr__(name: str) -> "t.Any":
+    if name == "data":
+        return _fetch_input_cached(_get_challenge_date())
+    else:
+        raise AttributeError(name)
+
+
+def _main() -> None:
+    date = _get_challenge_date()
+    script_path = _challenge_script_name(date)
+    with open(script_path, "r") as f:
+        script_src = f.read()
+    code = compile(source=script_src, filename=script_path, mode="exec")
+    exec(code, {}, {})
+
+
+def _challenge_script_name(date: "dt.date") -> str:
+    from pathlib import Path
+
+    return str(Path(__file__).parent / "days" / f"day{date.day}.py")
+
+
+def _read_session() -> str:
+    with open(".aoc-session", "r") as f:
+        return f.read().strip()
+
+
+def _get_challenge_date() -> "dt.date":
+    import datetime as dt
+    import os
+
+    today = dt.date.today()
+
+    day = int(os.environ.get("AOC_DAY", today.day))
+    year = int(os.environ.get("AOC_YEAR", today.year))
+    return dt.date(year=year, month=12, day=day)
+
+
+def _fetch_input_cached(date: "dt.date") -> str:
+    try:
+        with open(_input_cache_path(date), "r") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        input_data = _fetch_input(date)
+        _cache_input(date, input_data)
+        return input_data
+
+
+def _input_cache_path(date: "dt.date") -> str:
+    from pathlib import Path
+
+    cache_dir = Path(__file__).parent / ".aoc-cache"
+    cache_dir.mkdir(exist_ok=True)
+
+    return str(cache_dir / f"{date.year}-{date.day}.txt")
+
+
+def _fetch_input(date: "dt.date") -> str:
+    import requests
+
+    req = requests.get(
+        f"https://adventofcode.com/{date.year}/day/{date.day}/input",
+        cookies={"session": _read_session()},
+    )
+    req.raise_for_status()
+    return req.content.decode("ascii")
+
+
+def _cache_input(date: "dt.date", aoc_input: str) -> None:
+    with open(_input_cache_path(date), "w") as f:
+        f.write(aoc_input)
+
+
+if __name__ == "__main__":
+    _main()
