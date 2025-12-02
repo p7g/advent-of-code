98dd749b33d88c5e64b96120e25feb8052336949
Patrick Gingras <775.pg.12@gmail.com>
2025-12-01 20:37:33 -0500

    day 1

---
+++ 01.py
@@ -0,0 +1,28 @@
+from aoc import *
+
+ns = [int("".join(n)) * (-1 if d == "L" else 1) for d, *n in data.splitlines()]
+
+z = 0
+v = 50
+for n in ns:
+    v += n
+    v %= 100
+    z += v == 0
+
+print(z)
+
+
+z = 0
+v = 50
+for i, n in enumerate(ns):
+    if n > 0:
+        x, v = divmod(v + n, 100)
+        z += x
+    else:
+        x, y = divmod(n, -100)
+        z += x
+        z += abs(y) >= v and v != 0
+        v += y
+        v %= 100
+
+print(z)
